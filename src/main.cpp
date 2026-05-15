22ab5091cfedd2612370b4dc049df2f055805839
unknown <thextraodinaryco87@gmail.com>
2026-05-15 18:51:30 +0530

    Imgui setup

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 #include <raylib.h>
-
+#include <imgui.h>
+#include <rlImGui.h>
 int main()
 {
 	SetConfigFlags(FLAG_WINDOW_RESIZABLE);
 	InitWindow(1200, 800, "Game Window");
 	SetTargetFPS(60);
+	rlImGuiSetup(true);
 
 	std::cout << "Hello World\n";
 	std::cout << "New Repo initiated\n";
@@ -13,11 +15,21 @@ int main()
 	while (!WindowShouldClose())
 	{
 		BeginDrawing();
-		// ClearBackground(WHITE);
-		DrawRectangle(50, 50, 100, 100, {255, 0, 0, 127});
-		DrawRectangle(75, 75, 100, 100, {0, 255, 0, 127});
+		rlImGuiBegin();
+
+		ClearBackground(WHITE);
+		DrawText("Hello World", 190, 200, 20, {255, 0, 200, 255});
+
+		ImGui::Begin("test");
+
+		ImGui::Text("Hi there");
+		ImGui::Button("button");
+		
+		rlImGuiEnd();
 		EndDrawing();
 	}
+
+	rlImGuiShutdown();
 
 	CloseWindow();
 	return 0;
