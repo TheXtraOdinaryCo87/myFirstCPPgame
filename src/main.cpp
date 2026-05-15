9565b8a29b49842d8a9d09f43740ac97b04f2152
unknown <thextraodinaryco87@gmail.com>
2026-05-15 17:45:25 +0530

    imgui linked

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,7 @@
 int main()
 {
 	SetConfigFlags(FLAG_WINDOW_RESIZABLE);
-	InitWindow(800, 450, "Game Window");
+	InitWindow(1200, 800, "Game Window");
 	SetTargetFPS(60);
 
 	std::cout << "Hello World\n";
@@ -14,8 +14,8 @@ int main()
 	{
 		BeginDrawing();
 		// ClearBackground(WHITE);
-		DrawRectangle(50, 50, 100, 100, {255, 0, 0, 255});
-		DrawRectangle(75, 75, 100, 100, {0, 255, 0, 255});
+		DrawRectangle(50, 50, 100, 100, {255, 0, 0, 127});
+		DrawRectangle(75, 75, 100, 100, {0, 255, 0, 127});
 		EndDrawing();
 	}
 
